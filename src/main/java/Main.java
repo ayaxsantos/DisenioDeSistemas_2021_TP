ded617ca72b06899821bfc22c22a912470d177e4
Juyah <juanyarbuh123@gmail.com>
2021-05-11 15:35:38 -0300

    Reformat Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,22 +4,22 @@ import dominio.excepciones.ContraseniaDebilException;
 
 public class Main {
 
-    public static void main(String[] args){
-        generarUsuario("Usuario1","12345");
-        generarUsuario("Usuario2","Cont321Ho@13");
-        generarUsuario("Usuario3","password");
-        generarUsuario("Usuario4","kj2@3");
-        generarUsuario("Usuario4","Ho23@·Hola");
+    public static void main(String[] args) {
+        generarUsuario("Usuario1", "12345");
+        generarUsuario("Usuario2", "Cont321Ho@13");
+        generarUsuario("Usuario3", "password");
+        generarUsuario("Usuario4", "kj2@3");
+        generarUsuario("Usuario4", "Ho23@·Hola");
     }
 
-    public static void generarUsuario(String usuario, String contrasenia){
-        try{
-        new Usuario(usuario,contrasenia);
-        System.out.println("Se genero correctamente: " + usuario);}
-        catch (ContraseniaDebilException e){
-            System.out.println("No se pudo generar el usuario: " + usuario +", contrasenia debil");
-        }catch (ContraseniaCortaException e){
-            System.out.println("No se pudo generar el usuario: " + usuario +", contrasenia corta");
+    public static void generarUsuario(String usuario, String contrasenia) {
+        try {
+            new Usuario(usuario, contrasenia);
+            System.out.println("Se genero correctamente: " + usuario);
+        } catch (ContraseniaDebilException e) {
+            System.out.println("No se pudo generar el usuario: " + usuario + ", contrasenia debil");
+        } catch (ContraseniaCortaException e) {
+            System.out.println("No se pudo generar el usuario: " + usuario + ", contrasenia corta");
         }
 
     }
