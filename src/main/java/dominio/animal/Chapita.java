a0890f84d6944947ce1f30392d312998be4276c2
tbavutti <tomas.bavutti@gmail.com>
2021-08-13 09:35:22 -0300

    chapita

---
+++ src/main/java/dominio/animal/Chapita.java
@@ -0,0 +1,16 @@
+package dominio.animal;
+
+public class Chapita {
+
+    private final String codigoQR; //Para no generar conflicto lo puse como un string
+
+
+    public Chapita(String codigoQR) {
+        this.codigoQR = codigoQR;
+    }
+
+    public void formulario(String codigoQR){
+
+
+    }
+}
