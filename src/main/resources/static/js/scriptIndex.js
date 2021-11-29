00ca3ffbff8ddba1d9c7bf2b8ece6f74664acef5
ayaxsantos <ayax.asantos@gmail.com>
2021-11-29 12:12:14 -0300

    Logica cerrar sesion login cliente liviano

--- src/main/resources/static/js/scriptIndex.js
+++ src/main/resources/static/js/scriptIndex.js
@@ -4,7 +4,7 @@ const idUsuario = localStorage.getItem('idSesion');
 const idAdmin = localStorage.getItem('idAdmin');
 const idVol = localStorage.getItem('idVoluntario');
 
-if(idUsuario !== null || idAdmin !== null || idVol !== null){
+if(idUsuario || idAdmin || idVol){
     document.getElementById("botonIniciarSesion").style.display = "none";
     document.getElementById("botonRegistrarse").style.display = "none";
     document.getElementById("elementoCerrarSesion").style.display = "block";
@@ -16,4 +16,20 @@ function redireccionamiento(input){
     } else {
         location.href="../registrarPersona/registrarPersona.html"
     }
+}
+
+function cerrarSesion()
+{
+    appCerrarSesionVue.cerrarSesion();
+    localStorage.removeItem('idSesion');
+    localStorage.removeItem('idAdmin');
+    localStorage.removeItem('idVoluntario');
+    location.href = "../inicio/home.html"
+}
+
+function desconectar(auth) {
+    fetch('http://localhost:8080/usuarios/desconectar',
+        {headers: {"Authorization": auth}}).then(response => {
+        if (response.status !== 200) alert("Algo salio mal!!")
+    })
 }
--- src/main/resources/static/js/scriptLogin.js
+++ src/main/resources/static/js/scriptLogin.js
@@ -1,4 +1,6 @@
 //TODO: Actualizar los enlaces a la version deploy del cliente pesado!
+localStorage.removeItem('tipoDocumento');
+localStorage.removeItem('numeroDocumento');
 
 function solicitudInicioSesion(unaSolicitud) {
     return fetch("../usuarios/autenticar",
@@ -18,33 +20,33 @@ function solicitudInicioSesion(unaSolicitud) {
 
 function realizarSolicitud()
 {
-    let tipoDeUsuario =document.getElementById("tipoDeUsuario").value;
+    let tipoDeUsuario = document.getElementById("tipoDeUsuario").value;
     let unaSolicitud = {
         nombreUsuario: document.getElementById("nombreUsuario").value,
         contrasenia: document.getElementById("contrasenia").value
     }
-    let datos = solicitudInicioSesion(unaSolicitud);
-    if (datos.idSesion === "-1") {
-        alert("Usuario o contraseña invalido");
-    }
-    else
-    {
-        switch(tipoDeUsuario)
+    solicitudInicioSesion(unaSolicitud).then(datos => {
+        if(datos.idSesion === "-1" || datos.idSesion == undefined)
+            alert("Usuario o contraseña invalido");
+        else
         {
-            case "usuarios":
-                localStorage.setItem("idSesion", datos.idSesion);
-                window.location.href = "../home";
-                break;
-            case "administradores":
-                localStorage.setItem("idAdmin", datos.idSesion);
-                window.location.href = "../panelAdministrador/panelAdministrador.html";
-                break;
-            case "voluntarios":
-                localStorage.setItem("idVoluntario", datos.idSesion);
-                window.location.href = "../panelVoluntario/panelVoluntario.html";
-                break;
-            default:
-                alert("Error inesperado");
+            switch (tipoDeUsuario) {
+                case "usuarios":
+                    localStorage.setItem("idSesion", datos.idSesion);
+                    window.location.href = "../home";
+                    break;
+                case "administradores":
+                    localStorage.setItem("idAdmin", datos.idSesion);
+                    window.location.href = "../panelAdministrador/panelAdministrador.html";
+                    break;
+                case "voluntarios":
+                    localStorage.setItem("idVoluntario", datos.idSesion);
+                    window.location.href = "../panelVoluntario/panelVoluntario.html";
+                    break;
+                default:
+                    alert("Error inesperado");
+                    break;
+            }
         }
-    }
+    });
 }
